d1cb181239ba2ed6b042b0318567f2c1aea3debd
srivastavapiyush215135-wq <srivastavapiyush215135@gmail.com>
2026-07-13 17:48:11 +0530

    update frontend

--- frontend/app.py
+++ frontend/app.py
@@ -8,7 +8,7 @@ st.set_page_config(
     initial_sidebar_state="expanded"
 )
 
-BACKEND = "http://127.0.0.1:8000"
+BACKEND = "https://ai-interview-platform-s4j9.onrender.com/"
 with st.sidebar:
 
     st.title("🤖 AI Interview")
